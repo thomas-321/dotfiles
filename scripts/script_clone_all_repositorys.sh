9982b9b921c6370c6d0a5cb14657dfa9ade8b62d
Thomas van Vliet <git@thomasvanvliet.net>
2024-03-03 20:27:29 +0100

    Added repos

--- scripts/script_clone_all_repositorys.sh
+++ scripts/script_clone_all_repositorys.sh
@@ -4,6 +4,7 @@
 echo "Checking if the github cli tool gh is logged in..."
 
 if gh auth status &> /dev/null; then
+    echo "Check succesful.."
 else
     echo "User is not logged in to GitHub CLI. Please log in."
     exit 1
@@ -22,15 +23,17 @@ gh repo clone ti-projecten/TI01-PRJ04-1         $HOME/repos/02-school/year1/seme
 gh repo clone ti-projecten/TI02-LLC01           $HOME/repos/02-school/year1/semester2/TI02-LLC01
 gh repo clone ti-projecten/TI02-PRJ04-2         $HOME/repos/02-school/year1/semester2/TI02-PRJ04-2
 gh repo clone ti-projecten/TI03-HIS04-1         $HOME/repos/02-school/year1/semester3/TI03-HIS04-1
-gh repo clone my-hr-repos/TI-PROJECT3-4         $HOME/repos/02-school/year1/semester3/TI-PROJECT3-4
-gh repo clone my-hr-repos/TINPRO03-3            $HOME/repos/02-school/year1/semester3/TINPRO03-3
-gh repo clone my-hr-repos/project-34-landserver $HOME/repos/02-school/year1/semester3/project-34-landserver
-gh repo clone my-hr-repos/TI-TINPRO03-4         $HOME/repos/02-school/year1/semester4/TI-TINPRO03-4
-gh repo clone my-hr-repos/TINPRO025A            $HOME/repos/02-school/year2/semester1/TINPRO025A
-gh repo clone my-hr-repos/TINPRO045B            $HOME/repos/02-school/year2/semester1/TINPRO045B
-gh repo clone my-hr-repos/TI-project56-scr      $HOME/repos/02-school/year2/semester2/TI-project56-scr
-gh repo clone my-hr-repos/TINCOS01              $HOME/repos/02-school/year2/semester3/TINCOS01 
-gh repo clone my-hr-repos/TINPRO01-7            $HOME/repos/02-school/year2/semester3/TINPRO01-7
+gh repo clone hr-repos/TI-PROJECT3-4            $HOME/repos/02-school/year1/semester3/TI-PROJECT3-4
+gh repo clone hr-repos/TINPRO03-3               $HOME/repos/02-school/year1/semester3/TINPRO03-3
+gh repo clone hr-repos/project-34-landserver    $HOME/repos/02-school/year1/semester3/project-34-landserver
+gh repo clone hr-repos/TI-TINPRO03-4            $HOME/repos/02-school/year1/semester4/TI-TINPRO03-4
+gh repo clone hr-repos/TINPRO025A               $HOME/repos/02-school/year2/semester1/TINPRO025A
+gh repo clone hr-repos/TINPRO045B               $HOME/repos/02-school/year2/semester1/TINPRO045B
+gh repo clone hr-repos/TINPRJ0456               $HOME/repos/02-school/year2/semester2/TINPRJ0456
+gh repo clone hr-repos/TINCOS01                 $HOME/repos/02-school/year2/semester3/TINCOS01 
+gh repo clone hr-repos/TINPRO01-7               $HOME/repos/02-school/year2/semester3/TINPRO01-7
+gh repo clone hr-repos/TINPRJ0478               $HOME/repos/02-school/year2/semester3/TINPRJ0478
+gh repo clone hr-repos/TINNES                   $HOME/repos/02-school/year2/semester3/TINNES
 
 # personal
 gh repo clone thomas-321/PrivateWebsite         $HOME/repos/03-personal/PrivateWebsite
