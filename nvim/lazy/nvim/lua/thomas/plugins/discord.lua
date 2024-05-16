a3df1b4ba7fee8fa0587a58c0506df7af20af57b
Thomas van Vliet <git@thomasvanvliet.net>
2024-05-16 15:08:45 +0200

    Removed logging for nvim - discord plugin

--- nvim/lazy/nvim/lua/thomas/plugins/discord.lua
+++ nvim/lazy/nvim/lua/thomas/plugins/discord.lua
@@ -7,8 +7,8 @@ return {
 		local presence = require("presence")
 
 		presence.setup({
-			log_level = "debug",
-			-- -- General options
+			-- log_level = "debug",
+			-- General options
 			auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
 			neovim_image_text = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
 			-- main_image = "neovim", -- Main image display (either "neovim" or "file")
