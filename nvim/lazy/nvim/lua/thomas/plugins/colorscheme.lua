3e2e5f3b7ed5d05c88aa1cc57f84d0f659de84a4
Thomas van Vliet <git@thomasvanvliet.net>
2024-05-16 16:09:04 +0200

    Trying out Catppuccin theme in nvim

--- nvim/lazy/nvim/lua/thomas/plugins/colorscheme.lua
+++ nvim/lazy/nvim/lua/thomas/plugins/colorscheme.lua
@@ -1,12 +1,25 @@
+-- return {
+-- 	{
+-- 		"bluz71/vim-nightfly-guicolors",
+-- 		priority = 1000, -- make sure to load this before all the other start plugins
+-- 		config = function()
+-- 			-- load the colorscheme here
+-- 			vim.cmd([[colorscheme nightfly]])
+-- 		end,
+-- 	},
+-- }
+
 return {
-	{
-		"bluz71/vim-nightfly-guicolors",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme nightfly]])
-		end,
-	},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		local catppuccin = require("catppuccin")
+		catppuccin.setup({
+			flavour = "mocha",
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
 
 -- Another nice theme:
