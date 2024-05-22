be8ac84aa79d051623f5e0500d822e031c1213b3
Thomas van Vliet <git@thomasvanvliet.net>
2024-05-22 18:41:14 +0200

    Removed old keymaps and added new ones

--- nvim/lazy/nvim/lua/thomas/plugins/cheatsheet.lua
+++ nvim/lazy/nvim/lua/thomas/plugins/cheatsheet.lua
@@ -1,60 +1,76 @@
-local M = { 'smartinellimarco/nvcheatsheet.nvim' }
+local M = { "smartinellimarco/nvcheatsheet.nvim" }
 
 M.opts = {
-  -- Default header
-  header = {
-    "                                      ",
-    "                                      ",
-    "                                      ",
-    "█▀▀ █░█ █▀▀ ▄▀█ ▀█▀ █▀ █░█ █▀▀ █▀▀ ▀█▀",
-    "█▄▄ █▀█ ██▄ █▀█ ░█░ ▄█ █▀█ ██▄ ██▄ ░█░",
-    "                                      ",
-    "                                      ",
-    "                                      ",
-  },
-  -- Example keymaps (this doesn't create any)
-  keymaps = {
-    ['Oil'] = {
-      { 'Toggle oil (closes without saving)', '<leader>q' },
-      { 'Select entry', '⏎' },
-      { 'Select entry', 'l' },
-      { 'Go to parent', 'h' },
-      { 'Open vertical split', '⌃v' },
-      { 'Open horizontal split', '⌃x' },
-      { 'Go to current working directory', '.' },
-    },
-    ['Cmp'] = {
-      { 'Select entry', '⌃f' },
-      { 'Next result - Jump to next snippet placeholder', '⌃n' },
-      { 'Previous result - Jump to previous snippet placeholder', '⌃p' },
-      { 'Scroll up in preview', '⌃u' },
-      { 'Scroll down in preview', '⌃d' },
-      { 'Abort autocompletion', '⌃e' },
-    },
-    ['Comment'] = {
-      { 'Comment line toggle', 'gcc' },
-      { 'Comment block toggle', 'gbc' },
-      { 'Comment visual selection', 'gc' },
-      { 'Comment visual selection using block delimiters', 'gb' },
-      { 'Comment out text object line wise', 'gc<motion>' },
-      { 'Comment out text object block wise', 'gb<motion>' },
-      { 'Add comment on the line above', 'gcO' },
-      { 'Add comment on the line below', 'gco' },
-      { 'Add comment at the end of line', 'gcA' },
-    },
-  },
+	-- Default header
+	header = {
+		"                                      ",
+		"                                      ",
+		"                                      ",
+		"█▀▀ █░█ █▀▀ ▄▀█ ▀█▀ █▀ █░█ █▀▀ █▀▀ ▀█▀",
+		"█▄▄ █▀█ ██▄ █▀█ ░█░ ▄█ █▀█ ██▄ ██▄ ░█░",
+		"                                      ",
+		"                                      ",
+		"                                      ",
+	},
+	-- Example keymaps (this doesn't create any)
+	keymaps = {
+		["lsp"] = {
+			{ "Show definition, refrences)", "gR" },
+			{ "Go to Declaration", "gD" },
+			{ "Show definitions", "gd" },
+			{ "Show LSP implementations", "gi" },
+			{ "Show code actions", "<leader>ca" },
+			{ "Smart rename", "<leader>rn" },
+			{ "Show buffer diagnostics", "D" },
+			{ "Show line diagnostics", "d" },
+			{ "Show documentation for what is under cursor", "K" },
+		},
+		["Cmp"] = {
+			{ "Select entry", "⌃f" },
+			{ "Next result - Jump to next snippet placeholder", "⌃n" },
+			{ "Previous result - Jump to previous snippet placeholder", "⌃p" },
+			{ "Scroll up in preview", "⌃u" },
+			{ "Scroll down in preview", "⌃d" },
+			{ "Abort autocompletion", "⌃e" },
+		},
+		["Comment"] = {
+			{ "Comment line toggle", "gcc" },
+			{ "Comment block toggle", "gbc" },
+			{ "Comment visual selection", "gc" },
+			-- { 'Comment visual selection using block delimiters', 'gb' },
+			-- { 'Comment out text object line wise', 'gc<motion>' },
+			-- { 'Comment out text object block wise', 'gb<motion>' },
+			-- { 'Add comment on the line above', 'gcO' },
+			-- { 'Add comment on the line below', 'gco' },
+			-- { 'Add comment at the end of line', 'gcA' },
+		},
+		["useful"] = {
+			{ "Show all keymaps", "<F1>" },
+			{ "Format file or range", "<leader>mp" },
+			{ "Lint current file", "<leader>l" },
+		},
+		["Telescope"] = {
+			{ "Fuzzy find files in cwd", "<leader>ff" },
+			{ "Fuzzy find recent files", "<leader>fr" },
+			{ "Fuzzy find git files", "<leader>fg" },
+			{ "Find string in cwd", "<leader>fs" },
+			{ "Find string under cursor in cwd", "<leader>fc" },
+		},
+		["nvim"] = {
+			{ "Go back to last buffer", "<CR-o" },
+		},
+	},
 }
 
 function M.config(_, opts)
-  local nvcheatsheet = require('nvcheatsheet')
+	local nvcheatsheet = require("nvcheatsheet")
 
-  nvcheatsheet.setup(opts)
+	nvcheatsheet.setup(opts)
 
-  -- You can also close it with <Esc>
-  vim.keymap.set('n', '<F1>', nvcheatsheet.toggle)
+	-- You can also close it with <Esc>
+	vim.keymap.set("n", "<F1>", nvcheatsheet.toggle)
 end
 
 return M
 
 -- vim: ts=2 sts=2 sw=2 et
-
