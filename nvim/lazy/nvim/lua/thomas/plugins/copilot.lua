d27b82aacb3fcf3429d07664dff1d6502624c264
Thomas van Vliet <git@thomasvanvliet.net>
2024-05-24 00:03:55 +0200

    Changed keymap for auto-complete from <tab> to <CTRL-G>
    Fixed copilot hyjacking the tab key which prevents the tab key to be
    used to indent at the cursor

--- nvim/lazy/nvim/lua/thomas/plugins/copilot.lua
+++ nvim/lazy/nvim/lua/thomas/plugins/copilot.lua
@@ -1,6 +1,13 @@
 -- run ":Copilot setup" to login to github to use the plugin
 return {
-	{
-		"github/copilot.vim",
-	},
+    "github/copilot.vim",
+
+    config = function()
+
+    vim.keymap.set('i', '<C-G>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+    })
+    vim.g.copilot_no_tab_map = true
+    end,
 }
