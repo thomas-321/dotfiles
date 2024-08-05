ae186a4de537acf301130f2d4d311fe1c9e597e7
Thomas van Vliet <git@thomasvanvliet.net>
2024-08-05 19:23:30 +0200

    Added rust lsp to nvim

--- nvim/lazy/nvim/lua/thomas/plugins/lsp/lspconfig.lua
+++ nvim/lazy/nvim/lua/thomas/plugins/lsp/lspconfig.lua
@@ -216,6 +216,12 @@ return { -- configure html server
             on_attach = on_attach,
         })
 
+        -- configure rust server
+        lspconfig["rust_analyzer"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+
         -- configure lua server (with special settings)
         lspconfig["lua_ls"].setup({
             capabilities = capabilities,
