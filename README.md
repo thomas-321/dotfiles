7543381763f1106f447a8781dd15af728a96870d
Thomas van Vliet <git@thomasvanvliet.net>
2025-09-08 11:17:51 +0200

    Add nvchad config

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotfiles
 My dotfiles
+
+
+# Neovim
+I use neovim as a code editor. The configuration is heavily based on [NvChad](https://github.dev/NvChad/NvChad) and alterd so that all configuration, 
+except [nvchad/base64](https://github.com/NvChad/base46) and [nvchad/ui](https://github.com/NvChad/ui), is inside this repo.
+
---
+++ nvim/nvchad/lua/plugins/init.lua
@@ -0,0 +1,261 @@
+return {
+	"nvim-lua/plenary.nvim",
+
+	{
+		"nvchad/base46",
+		build = function()
+			require("base46").load_all_highlights()
+		end,
+	},
+
+	{
+		"nvchad/ui",
+		lazy = false,
+		config = function()
+			require("nvchad")
+		end,
+	},
+
+	"nvzone/volt",
+	"nvzone/menu",
+	{ "nvzone/minty", cmd = { "Huefy", "Shades" } },
+
+	{
+		"nvim-tree/nvim-web-devicons",
+		opts = function()
+			dofile(vim.g.base46_cache .. "devicons")
+			return { override = require("nvchad.icons.devicons") }
+		end,
+	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "User FilePost",
+		opts = {
+			indent = { char = "│", highlight = "IblChar" },
+			scope = { char = "│", highlight = "IblScopeChar" },
+		},
+		config = function(_, opts)
+			dofile(vim.g.base46_cache .. "blankline")
+
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+			require("ibl").setup(opts)
+
+			dofile(vim.g.base46_cache .. "blankline")
+		end,
+	},
+
+	-- file managing , picker etc
+	{
+		"nvim-tree/nvim-tree.lua",
+		cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+		opts = function()
+			return require("configs.nvimtree")
+		end,
+	},
+
+	{
+		"folke/which-key.nvim",
+		keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+		cmd = "WhichKey",
+		opts = function()
+			dofile(vim.g.base46_cache .. "whichkey")
+			return {}
+		end,
+	},
+
+	-- formatting!
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = { lua = { "stylua" } },
+		},
+	},
+
+	-- git stuff
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "User FilePost",
+		opts = function()
+			return require("configs.gitsigns")
+		end,
+	},
+
+	-- lsp stuff
+	{
+		"mason-org/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+		opts = function()
+			return require("configs.mason")
+		end,
+	},
+
+	{
+		"neovim/nvim-lspconfig",
+		event = "User FilePost",
+		config = function()
+			require("configs.lspconfig").defaults()
+		end,
+	},
+
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^6", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+
+	{
+		"saecki/crates.nvim",
+		ft = { "toml" },
+		tag = "stable",
+		config = function()
+			require("crates").setup({
+				completion = {
+					cmp = {
+						enabled = true,
+					},
+				},
+			})
+			require("cmp").setup.buffer({
+				sources = { { name = "crates" } },
+			})
+		end,
+	},
+
+	{
+		"nvim-flutter/flutter-tools.nvim",
+        ft = { "dart" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = true,
+	},
+
+	-- load luasnips + cmp related in insert mode only
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				-- snippet plugin
+				"L3MON4D3/LuaSnip",
+				dependencies = "rafamadriz/friendly-snippets",
+				opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+				config = function(_, opts)
+					require("luasnip").config.set_config(opts)
+					require("configs.luasnip")
+				end,
+			},
+
+			-- autopairing of (){}[] etc
+			-- {
+			-- 	"windwp/nvim-autopairs",
+			-- 	opts = {
+			-- 		fast_wrap = {},
+			-- 		disable_filetype = { "TelescopePrompt", "vim" },
+			-- 	},
+			-- 	config = function(_, opts)
+			-- 		require("nvim-autopairs").setup(opts)
+			--
+			-- 		-- setup cmp for autopairs
+			-- 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			-- 		require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			-- 	end,
+			-- },
+
+			-- cmp sources plugins
+			{
+				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-buffer",
+				"https://codeberg.org/FelipeLema/cmp-async-path.git",
+			},
+		},
+		opts = function()
+			return require("configs.cmp")
+		end,
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		cmd = "Telescope",
+		opts = function()
+			return require("configs.telescope")
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+		build = ":TSUpdate",
+		opts = function()
+			return require("configs.treesitter")
+		end,
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = true,
+		keys = { -- load the plugin only when using it's keybinding:
+			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		},
+	},
+	{
+		"lervag/vimtex",
+		ft = { "tex", ""},
+		config = function()
+			-- Enable vimtex plugin with concealment
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_syntax_enabled = 1
+			vim.g.vimtex_syntax_conceal = {
+				accents = 2,
+				cites = 2,
+				fancy = 2,
+				greek = 2,
+				math_bounds = 2,
+				math_delimiters = 2,
+				math_fracs = 2,
+				math_super_sub = 2,
+				math_symbols = 2,
+				sections = 2,
+				styles = 2,
+				trailing_whitespace = 0,
+			}
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+}
